eb844660faae2321a9dd81a792f973067fefc207
Roberto Castro <49725075+Roark98@users.noreply.github.com>
2022-09-08 16:28:57 -0500

    Add files via upload

---
+++ Train.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import json
+from Utils import CustomDataset, dice_score, get_model, dice_score_2
+import numpy as np
+import os
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import argparse
+
+
+def train_model(model_name, model, criterion, optimizer, num_epochs=3):
+    tr_losses = {}  #Training losses
+    ts_losses = {}  #Test losses
+    ts_dice = {}    #Test Dice Scores
+    #Epochs
+    for epoch in range(num_epochs):
+        print('-' * 100)
+        print('Epoch {}/{}'.format(epoch+1, num_epochs))
+
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+                epoch_mean_dice = 0
+
+            running_loss = 0.0
+            #Batches
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                #Model output
+                outputs = model(inputs.float())
+                if model_name=='TransUNet':
+                    outputs = torch.sigmoid(outputs)
+                loss = criterion(outputs, labels.float())
+
+                if phase == 'train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+                else:
+                    out_cut = np.copy(outputs.data.cpu().numpy())
+                    #Applying Thresholding
+                    out_cut[np.nonzero(out_cut < 0.5)] = 0.0
+                    out_cut[np.nonzero(out_cut >= 0.5)] = 1.0
+                    #Dice Score Calculation
+                    batch_dice = dice_score(
+                        out_cut, labels.cpu().numpy()).item()
+                    epoch_mean_dice += batch_dice
+
+                running_loss += loss.item() * inputs.size(0)
+            epoch_loss = running_loss / len(image_datasets[phase])
+
+            #Storing loss and dice score
+            if phase == 'train':
+                tr_losses[epoch] = epoch_loss
+            else:
+                ts_losses[epoch] = epoch_loss
+                epoch_mean_dice = epoch_mean_dice/len(dataloaders['test'])
+
+                #Checking if a new best model is found
+                if len(ts_dice) == 0 or epoch_mean_dice>max(ts_dice.values()):
+                    print('-----New best model found! Saving...')
+                    best_model = model
+
+                ts_dice[epoch] = epoch_mean_dice
+                print("*****Epoch Test Dice Score: ", epoch_mean_dice)
+
+            print('*****Phase: {} - Loss: {:.4f}'.format(phase, epoch_loss))
+
+    print('-' * 100)
+    print("*****Best test loss: "+str(min(ts_losses.values())))
+
+    #Saving releveant records
+    if os.path.isdir('./results')==False:
+        os.mkdir('./results')
+    if os.path.isdir('./results/'+model_name)==False:
+        os.mkdir('./results/'+model_name)
+
+    torch.save(best_model.state_dict(), './results/'+model_name+'/trained_model.pth')
+
+    with open('./results/'+model_name+'/train_losses.json', 'w') as f:
+        json.dump(tr_losses, f)
+    with open('./results/'+model_name+'/test_losses.json', 'w') as f:
+        json.dump(ts_losses, f)
+    with open('./results/'+model_name+'/Dice_scores.json', 'w') as f:
+        json.dump(ts_dice, f)
+
+    #Plotting
+    plt.plot(ts_losses.keys(),ts_losses.values())
+    plt.plot(tr_losses.keys(),tr_losses.values())
+    plt.title('Test Loss Evolution')
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid()
+    plt.show()
+
+    return model
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', type=str,
+                        default='UNet', help='Model to use', choices=['UNet', 'TransUNet'])
+    parser.add_argument('--epochs', type=int, default=30,
+                        help='Number of epochs')
+
+    args = parser.parse_args()
+
+    #Data paths
+    path_imgs = './Task02_Heart/ImgTr'    #Images
+    path_lb = './Task02_Heart/LblTr'    #Labels
+
+    images = os.listdir(path_imgs)
+    labels = os.listdir(path_lb)
+
+    #Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(images,labels, test_size=0.2, shuffle=False)
+    print('Train len: ',len(X_train))
+    print('Test len: ',len(X_test))
+
+
+    #Datasets and Dataloaders
+    transformations = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    tr_dataset = CustomDataset(path_imgs, path_lb, X_train, y_train, transformations)
+    ts_dataset = CustomDataset(path_imgs, path_lb, X_test, y_test, transformations)
+
+    image_datasets = {'train': tr_dataset, 'test': ts_dataset}
+
+    tr_dataloader = torch.utils.data.DataLoader(
+        tr_dataset, batch_size=10, shuffle=True, num_workers=3)
+    ts_dataloader = torch.utils.data.DataLoader(
+        ts_dataset, batch_size=10, shuffle=True, num_workers=3)
+
+    dataloaders = {'train': tr_dataloader, 'test': ts_dataloader}
+
+    #Model and Hyperparameters
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+
+    device = torch.device('cpu')
+
+    print("Selected device: ", device)
+
+    model = get_model(args.model).to(device)
+
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00006)
+
+    #Train and Save
+    train_model = train_model(args.model, model, criterion, optimizer, args.epochs)
+    
---
+++ Utils.py
@@ -0,0 +1,67 @@
+from torch.utils.data import Dataset
+import torch
+import os
+import cv2
+from TransUNet.networks.vit_seg_modeling import VisionTransformer as ViT_seg
+from TransUNet.networks.vit_seg_modeling import CONFIGS as CONFIGS_ViT_seg
+
+def dice_score_2(pred,true_mask):
+
+    pred = torch.from_numpy(pred)
+    true_mask = torch.from_numpy(true_mask)
+
+    pred = pred.view(-1)
+    mask = true_mask.view(-1)
+    
+    pred = (pred>0.5).float()
+    mask = mask.float()
+
+    intersection = (pred * mask).sum() 
+
+    dice = (intersection *2.0) / (pred.sum() + mask.sum())
+
+    return dice
+
+def dice_score(pred, label):
+    intersection = 2.0 * (pred * label).sum()
+    union = pred.sum() + label.sum()
+    if pred.sum() == 0 and label.sum() == 0:
+        return 1.
+    return intersection / union
+
+class CustomDataset(Dataset):
+  def __init__(self, pathImg, pathLbl, images, labels, transform=None):
+
+    self.images = images
+    self.labels = labels
+    self.pathImg = pathImg
+    self.pathLbl = pathLbl
+
+    self.transform = transform
+    
+  def __len__(self):
+    return len(self.images)
+
+  def __getitem__(self, idx):
+    img_file_path = os.path.join(self.pathImg, self.images[idx])
+    lbl_file_path = os.path.join(self.pathLbl, self.labels[idx])
+
+    img = cv2.imread(img_file_path, 0)
+    mask = cv2.imread(lbl_file_path, 0)/255
+
+    #Pytorch Transformations
+    img = self.transform(img)
+    mask = self.transform(mask)
+
+    return img, mask
+
+def get_model(model_name):
+  if model_name=='UNet':
+    return torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',
+                           in_channels=1, out_channels=1, init_features=32, pretrained=False)
+  elif model_name=='TransUNet':
+    config_vit = CONFIGS_ViT_seg['R50-ViT-B_16']
+    config_vit.n_classes = 1
+    config_vit.n_skip = 3
+
+    return ViT_seg(config_vit, img_size=320, num_classes=config_vit.n_classes)
